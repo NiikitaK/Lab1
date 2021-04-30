97b7903bb768c3ee568ae77f8d0a51793890c481
NiikitaK <82287490+NiikitaK@users.noreply.github.com>
2021-04-30 12:59:25 +0400

    Add controller

--- src/com/company/controller/Controller.java
+++ src/com/company/controller/Controller.java
@@ -1,9 +1,45 @@
 package com.company.controller;
 
 import java.awt.*;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
-public class Controller {
-    void execute() {
+import static org.graalvm.compiler.nodeinfo.Verbosity.Id;
+
+class Controller {
+
+    private Map map;
+
+    public static History create(int id,
+                                 Date date,
+                                 String user,
+                                 String result) {
+        History newbie = ClassSpecializer.factory.create( int id,
+        Date date,
+        String user,
+        String result);
+        map.put(newbie.getId, newbie);
+    }
+
+    public void remove(id) {
+        map.remove(id);
+    }
+
+    public int get(id) {
+        return map.get(id);
+    }
+
+    public History getAll() {
+        return map.values();
+    }
+
+    public History update(int id,
+                          Date date,
+                          String user,
+                          String result) {
+//like for create at this moment
     }
 }
